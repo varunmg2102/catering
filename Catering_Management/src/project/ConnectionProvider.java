8c0e85c847626675f483729eb1a4271611e77af8
Varun M G <92241895+varunmg2102@users.noreply.github.com>
2023-02-09 23:29:00 +0530

    Add files via upload

---
+++ Catering_Management/src/project/ConnectionProvider.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package project;
+import java.sql.Connection;
+import java.sql.DriverManager;
+/**
+ *
+ * @author Varun M G
+ */
+public class ConnectionProvider {
+    public static Connection getCon()
+    {
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/catering","root","Varun@21");
+            return con;
+        }
+        catch(Exception e)
+        {
+            return null;
+        }
+    }
+}
---
+++ Catering_Management/src/project/tables.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package project;
+import java.sql.*;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Varun M G
+ */
+public class tables {
+    public static void main(String[] args)
+    {
+        Connection con=null;
+        Statement st=null;
+        try
+        {
+            con=ConnectionProvider.getCon();
+            st=con.createStatement();
+            st.executeUpdate("Create table users(name varchar(200),password varchar(200),status varchar(20))");
+            JOptionPane.showMessageDialog(null,"Table created sucessfully");
+        }
+        catch(Exception e)
+        {
+            JOptionPane.showMessageDialog(null, e);
+        }
+        finally
+        {
+             try
+        {
+            con.close();
+            st.close();
+        }
+        catch(Exception e)
+        {
+            
+        }
+        }
+    }
+}
